99552aca920134b72de310cee5acf3e3aa8bdb31
shumskiy <shumskiparents@gmail.com>
2026-04-09 19:38:08 +0200

    page

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,23 +1,22 @@
+
 import type { Metadata } from "next";
 import { fetchNoteById } from "@/lib/api";
 
 interface Props {
-  params: Promise<{ id: string }>;
+  params: { id: string }; 
 }
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const { id } = await params;
-  const note = await fetchNoteById(id);
 
-  
-  
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const note = await fetchNoteById(params.id);
+
   return {
     title: note.title,
     description: note.content || "",
     openGraph: {
       title: note.title,
       description: note.content || "",
-      url: `https://notehub.app/notes/${id}`,
+      url: `https://notehub.app/notes/${params.id}`,
       images: [
         {
           url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
@@ -25,4 +24,16 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
       ],
     },
   };
+}
+
+
+export default async function Page({ params }: Props) {
+  const note = await fetchNoteById(params.id);
+
+  return (
+    <div>
+      <h1>{note.title}</h1>
+      <p>{note.content}</p>
+    </div>
+  );
 }
